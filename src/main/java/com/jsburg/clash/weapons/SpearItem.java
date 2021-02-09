8618ede1c13f6780ce1f86e3bb5b0dbd0b02e0df
jsburg <dizzydooble@gmail.com>
2021-02-09 04:18:43 -0500

    Add basic charge attack to the spear
    *Adds AttackHelper, just covers some stuff that will probably be helpful across multiple weapons.
    *Horizonally flipped the Spear texture. It looks pointy now.

--- src/main/java/com/jsburg/clash/weapons/SpearItem.java
+++ src/main/java/com/jsburg/clash/weapons/SpearItem.java
@@ -5,14 +5,39 @@ import com.jsburg.clash.registry.AllSounds;
 import com.jsburg.clash.weapons.util.AttackHelper;
 import com.jsburg.clash.weapons.util.WeaponItem;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.*;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.ForgeMod;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class SpearItem extends WeaponItem {
+
+    private final int stabLengthBonus = 2;
+    private final int stabDamageBonus = 2;
 
     public SpearItem(int attackDamage, float attackSpeed, Item.Properties properties) {
         super(attackDamage, attackSpeed, properties);
@@ -28,4 +53,96 @@ public class SpearItem extends WeaponItem {
 //            AttackHelper.playSound(player, AllSounds.WEAPON_SPEAR_STAB.get());
 //        }
     }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+
+    public String formatNumber(float n) {
+        String s = String.valueOf(n);
+        //Regex magic I got off stackoverflow, gets rid of trailing 0's.
+        s = s.contains(".") ? s.replaceAll("0*$", "").replaceAll("\\.$", "") : s;
+        return s;
+    }
+
+    public ITextComponent getBonusText(String langString, float bonus) {
+        StringTextComponent text = new StringTextComponent((bonus > 0) ? " +" : " -");
+        text.appendString(formatNumber(bonus) + " ");
+        text.append(new TranslationTextComponent(langString));
+        text.mergeStyle(bonus > 0 ? TextFormatting.DARK_GREEN : TextFormatting.RED);
+        return text;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        super.addInformation(stack, worldIn, tooltip, flagIn);
+        tooltip.add((new TranslationTextComponent("item.clash.spear.when_charged")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add(getBonusText("item.clash.spear.charge_range_bonus", stabLengthBonus));
+        tooltip.add((new TranslationTextComponent("item.clash.spear.fully_charged")).mergeStyle(TextFormatting.GRAY));
+        tooltip.add(getBonusText("attribute.name.generic.attack_damage", stabDamageBonus));
+//        ITextComponent dmg = new StringTextComponent(" +0-" + formatNumber(stabDamageBonus) + " ")
+//                .append(new TranslationTextComponent("attribute.name.generic.attack_damage"))
+//                .mergeStyle(TextFormatting.DARK_GREEN);
+//        tooltip.add(dmg);
+//        tooltip.add((new TranslationTextComponent("item.clash.spear.cannot_crit")).mergeStyle(TextFormatting.RED));
+    }
+
+    public int getUseDuration(ItemStack stack) {
+        return 720000;
+    }
+
+    private int getMaxCharge() {
+        return 24;
+    }
+
+    public void onPlayerStoppedUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
+        if (entityLiving instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity)entityLiving;
+            int chargeTime = getUseDuration(stack) - timeLeft;
+            float chargePercent = Math.min((float)chargeTime/getMaxCharge(), 1);
+            Clash.LOGGER.debug(chargeTime);
+            if (chargeTime >= 10) {
+                player.addStat(Stats.ITEM_USED.get(this));
+                AttackHelper.playSound(player, AllSounds.WEAPON_SPEAR_STAB.get());
+                player.swingArm(player.getActiveHand());
+
+                double stabLength = player.getAttributeValue(ForgeMod.REACH_DISTANCE.get()) + stabLengthBonus;
+                Vector3d look = player.getLookVec();
+                Vector3d endVec = look.scale(stabLength);
+                Vector3d eyePos = player.getEyePosition(1.0F);
+                Vector3d endPos = eyePos.add(endVec);
+                AxisAlignedBB boundingBox = player.getBoundingBox().expand(endVec).grow(1);
+                Predicate<Entity> predicate = (e) -> !e.isSpectator() && e.canBeCollidedWith();
+                EntityRayTraceResult rayTraceResult = ProjectileHelper.rayTraceEntities(player, eyePos, endPos, boundingBox, predicate, 1000);
+
+                if (rayTraceResult != null) {
+                    Entity target = rayTraceResult.getEntity();
+
+                    //Calls Forge's attacking hook, this is definitely a direct attack.
+                    boolean canAttack = AttackHelper.fullAttackEntityCheck(player, target);
+                    if (canAttack) {
+                        ItemStack spear = player.getActiveItemStack();
+                        float damage = this.getAttackDamage() + (chargeTime > 20 ? stabDamageBonus : 0);
+                        player.resetCooldown();
+
+                        AttackHelper.attackEntity(player, target, damage);
+                        AttackHelper.damageTool(player, target, spear);
+                        AttackHelper.playSound(player, SoundEvents.ENTITY_PLAYER_ATTACK_STRONG);
+
+                        player.addExhaustion(0.2f);
+
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+        playerIn.setActiveHand(handIn);
+        return ActionResult.resultConsume(stack);
+    }
+
 }
--- src/main/java/com/jsburg/clash/weapons/util/AttackHelper.java
+++ src/main/java/com/jsburg/clash/weapons/util/AttackHelper.java
@@ -2,12 +2,26 @@ package com.jsburg.clash.weapons.util;
 
 import com.jsburg.clash.registry.AllSounds;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonPartEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.world.server.ServerWorld;
 
 public class AttackHelper {
     public static boolean canAttackEntity(PlayerEntity player, Entity targetEntity) {
         return (targetEntity.canBeAttackedWithItem() && !targetEntity.hitByEntity(player));
+    }
+    public static boolean fullAttackEntityCheck(PlayerEntity player, Entity targetEntity) {
+        if (!net.minecraftforge.common.ForgeHooks.onPlayerAttackTarget(player, targetEntity)) return false;
+        return canAttackEntity(player, targetEntity);
     }
     public static boolean weaponIsCharged(PlayerEntity player) {
         return player.getCooledAttackStrength(0.5F) > 0.9F;
@@ -17,5 +31,50 @@ public class AttackHelper {
     }
     public static void playSound(PlayerEntity player, SoundEvent sound, float volume, float pitch) {
         player.world.playSound(null, player.getPosX(), player.getPosY(), player.getPosZ(), sound, player.getSoundCategory(), volume, pitch);
+    }
+
+    public static void damageTool(PlayerEntity player, Entity target, ItemStack weapon) {
+        damageTool(player, target, weapon, player.getActiveHand());
+    }
+    //Referenced from net.minecraft.entity.player.PlayerEntity.attackTargetEntityWithCurrentItem
+    public static void damageTool(PlayerEntity player, Entity target, ItemStack weapon, Hand hand) {
+        if (target instanceof EnderDragonPartEntity) {
+            target = ((EnderDragonPartEntity)target).dragon;
+        }
+        if (!player.world.isRemote && target instanceof LivingEntity) {
+            ItemStack copy = weapon.copy();
+            weapon.hitEntity((LivingEntity) target, player);
+            if (weapon.isEmpty()) {
+                net.minecraftforge.event.ForgeEventFactory.onPlayerDestroyItem(player, copy, hand);
+                player.setHeldItem(hand, ItemStack.EMPTY);
+            }
+        }
+    }
+
+    //Hits entity, spawns particles, adds stats.
+    public static void attackEntity(PlayerEntity player, Entity target, float damage) {
+
+        float lastHealth = 0;
+        if (target instanceof LivingEntity) {
+            lastHealth = ((LivingEntity) target).getHealth();
+        }
+        target.attackEntityFrom(DamageSource.causePlayerDamage(player), damage);
+
+        if (target instanceof LivingEntity) {
+            float healthDifference = lastHealth - ((LivingEntity) target).getHealth();
+            player.addStat(Stats.DAMAGE_DEALT, Math.round(healthDifference * 10));
+
+            if (player.world instanceof ServerWorld && healthDifference > 2.0F) {
+                int k = (int)(healthDifference * 0.5D);
+                ((ServerWorld)player.world)
+                        .spawnParticle(
+                                ParticleTypes.DAMAGE_INDICATOR,
+                                target.getPosX(),
+                                target.getPosYHeight(0.5),
+                                target.getPosZ(),
+                                k, 0.1, 0.0, 0.1, 0.2);
+            }
+        }
+
     }
 }
